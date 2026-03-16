302fb8d3f9e05446b19b8ec27cea854860c794c1
errorhandlerst <patelt707@gmail.com>
2026-03-16 19:42:01 -0400

    Initial Commit

---
+++ TechConnectTests/TechConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  TechConnectTests.swift
+//  TechConnectTests
+//
+//  Created by Twinkle on 3/16/26.
+//
+
+import Testing
+@testable import TechConnect
+
+struct TechConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
